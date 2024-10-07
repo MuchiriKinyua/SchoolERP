39746bcc5c0817cb51f66ed706681184d880ddfd
muchirikinyua <muchirikinyua2000@gmail.com>
2024-10-07 10:55:12 +0300

    home page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Auth::routes(['verify' => true]);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
